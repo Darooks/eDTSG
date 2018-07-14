3296b524c1c54ed1ca324e29470925f3023b8d7b
Karol Jurczenia <karol.jurczenia@intel.com>
2018-07-14 15:12:54 +0200

    Added Domain class. Range is circle now. Added lane attribute to vehicle

---
+++ Domain.py
@@ -0,0 +1,13 @@
+import eDTSGSimulation
+import Utils
+
+class Domain:
+    def __init__(self,
+                 mid_x,
+                 mid_y,
+                 angle,
+                 points, lane):
+        self.mid_x = mid_x
+        self.mid_y = mid_y
+        self.angle = angle
+        self.lane = lane
